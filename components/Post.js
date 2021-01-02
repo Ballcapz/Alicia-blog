f5d74761a2ec242ad57ba0b21751f180318e3cdc
Zach Johnson <zlg23johnson@gmail.com>
2021-01-02 14:44:30 -0600

    Added posts

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,49 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+
+const Bold = ({ children }) => <span className="bold">{children}</span>;
+
+
+const options = {
+  renderMark: {
+    [MARKS.BOLD]: text => <Bold>{text}</Bold>
+  },
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node) => {
+      const { title, description, file } = node.data.target.fields;
+      const mimeType = file.contentType
+      const mimeGroup = mimeType.split('/')[0]
+
+      switch (mimeGroup) {
+        case 'image':
+          return <img
+            className="post-image"
+            title={ title ? title : null}
+            alt={description ?  description : null}
+            src={file.url}
+          />
+        case 'application':
+          return <a
+            alt={description ?  description : null}
+            href={file.url}
+            >{ title ? title : file.details.fileName }
+          </a>
+        default:
+          return <span style={{backgroundColor: 'red', color: 'white'}}> {mimeType} embedded asset </span>
+      }
+    },
+  }
+};
+
 function Post({ content, title, description}) {
+
+    const contentDoc = content;
     
     return (
         <div className="post">
             <h1 className="title">{title}</h1>
             <p>{description}</p>
-            <div className="content">
-                {content}
-            </div>
+            {contentDoc && documentToReactComponents(contentDoc, options)}
         </div>
     )
 }
